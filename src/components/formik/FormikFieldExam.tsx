79afa132aef727a0b9c167dcdb174ad0f6a47884
silverbeen <kub9722@gmail.com>
2022-07-26 18:54:27 +0900

    fix: validationSchema 추가

--- src/components/formik/FormikFieldExam.tsx
+++ src/components/formik/FormikFieldExam.tsx
@@ -1,49 +1,14 @@
+import * as Yup from "yup";
+import styled from "styled-components";
 import { Form, Formik, Field } from "formik";
 import { TextField, Button, Checkbox } from "@material-ui/core";
-import styled from "styled-components";
-import LabeledRadio from "./LabeledRadio";
-import LabeledCheckbox from "./LabeledCheckbox";
+import LabeledRadio from "./lebel/LabeledRadio";
+import LabeledCheckbox from "./lebel/LabeledCheckbox";
+import YUP_SCHEMA from "../../constants/yupSchema";
 
 const FormikFieldExam = () => {
   return (
     <Container>
-      <h3>Formik 기본</h3>
-      <Formik
-        initialValues={{ username: "", password: "" }} // 필수로 들어가야 하는 요소
-        onSubmit={(data, { setSubmitting }) => {
-          setSubmitting(true);
-          // 비동기 통신을 설정해주면, isSubmitting을 통해 현재 비동기 통신중인지 확인할 수 있다.
-
-          console.log(data);
-          setSubmitting(false);
-        }}
-      >
-        {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-          <>
-            <form onSubmit={handleSubmit}>
-              <div>
-                <TextField
-                  name="username"
-                  value={values.username}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <TextField
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                />
-              </div>
-              {/* isSubmitting 을 사용하여 true일 때는 양식을 두 번 보내지 않도록 버튼을 disabeld 시킨다. */}
-              <Button type="submit" disabled={isSubmitting}>
-                Submit
-              </Button>
-            </form>
-          </>
-        )}
-      </Formik>
-
       <h3>입력하는 input이 많은 경우 자동으로 넣어주는 인풋을 사용한다. </h3>
       <p>ex) {"<Formik /> <Field /> <Form />"} 태그들</p>
 
@@ -54,34 +19,41 @@ const FormikFieldExam = () => {
           isNoob: false,
           skills: [],
         }}
+        validationSchema={Yup.object().shape({
+          username: YUP_SCHEMA.USER_NAME,
+          password: YUP_SCHEMA.PASSWORD,
+          skills: YUP_SCHEMA.CHECK_BOX,
+        })}
         onSubmit={(data, { setSubmitting, resetForm }) => {
           setSubmitting(true);
           console.log(data);
-          setSubmitting(false);
 
+          setSubmitting(false);
           resetForm();
         }}
       >
-        {({ isSubmitting }) => (
+        {({ isSubmitting, touched, errors }) => (
           <Form>
             <div>
               <Field name="username" as={TextField} />
+              {touched.username && errors.username ? (
+                <ErrorText>{errors.username}</ErrorText>
+              ) : null}
             </div>
             {/* Field의 name만으로도 handleSubmitm value, handleChange, handleBlur가 전부 자동 설정되고 as 를 통해 어떤 스타일의 input을 사용할 건지 정의 해주면 된다.  */}
-            <Field name="password" as={TextField} />
+            <div>
+              <Field name="password" type="password" as={TextField} />
+              {touched.password && errors.password ? (
+                <ErrorText>{errors.password}</ErrorText>
+              ) : null}
+            </div>
 
             <h3> checkbox / radio example</h3>
 
             <Field name="isNoob" as={Checkbox} />
 
+            <h3>상태관리</h3>
             <div>
-              <LabeledCheckbox
-                name="skills"
-                value="React"
-                label="React"
-                type="checkbox"
-              />
-
               <LabeledCheckbox
                 name="skills"
                 value="Redux"
@@ -91,37 +63,51 @@ const FormikFieldExam = () => {
 
               <LabeledCheckbox
                 name="skills"
-                value="Webpack"
-                label="Webpack"
+                value="Redux-saga"
+                label="Redux-saga"
                 type="checkbox"
               />
 
               <LabeledCheckbox
                 name="skills"
-                value="Typescript"
-                label="Typescript"
+                value="react-query"
+                label="react-query"
                 type="checkbox"
               />
+
+              <LabeledCheckbox
+                name="skills"
+                value="Recoil"
+                label="Recoil"
+                type="checkbox"
+              />
+
+              <LabeledCheckbox
+                name="skills"
+                value="zustand"
+                label="zustand"
+                type="checkbox"
+              />
+              {touched.skills && errors.skills ? (
+                <ErrorText>{errors.skills}</ErrorText>
+              ) : null}
             </div>
+
+            <h3>사용언어</h3>
             <div>
               <LabeledRadio
-                name="nation"
-                value="Korea"
-                label="Korea"
+                name="lang"
+                value="Javascript"
+                label="Javascript"
                 type="radio"
               />
               <LabeledRadio
-                name="nation"
-                value="Japan"
-                label="Japan"
+                name="lang"
+                value="Typescript"
+                label="Typescript"
                 type="radio"
               />
-              <LabeledRadio
-                name="nation"
-                value="China"
-                label="China"
-                type="radio"
-              />
+              <LabeledRadio name="lang" value="All" label="All" type="radio" />
             </div>
 
             <Button type="submit" disabled={isSubmitting}>
@@ -135,7 +121,7 @@ const FormikFieldExam = () => {
 };
 
 const Container = styled.div`
-  width: 500px;
+  width: 600px;
   margin: 100px auto;
 
   & label {
@@ -143,4 +129,9 @@ const Container = styled.div`
   }
 `;
 
+const ErrorText = styled.p`
+  color: #ed7e7e;
+  font-size: 14px;
+  font-weight: bold;
+`;
 export default FormikFieldExam;
