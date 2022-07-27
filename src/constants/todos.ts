d899107feaf57908e7f5ba0c0d524e247bec418e
Eunbin Kang <kub9722@gmail.com>
2022-07-27 18:42:45 +0900

    feat: dnd dummy 추가

---
+++ src/constants/todos.ts
@@ -0,0 +1,8 @@
+export const todos = [
+  { id: "1", title: "공부" },
+  { id: "2", title: "숙제" },
+  { id: "3", title: "과제" },
+  { id: "4", title: "학교" },
+  { id: "5", title: "회사" },
+  { id: "6", title: "시험" },
+];
