2a96ac2d61ffcead3a7ee714ff68c4c1735abfe9
Eunbin Kang <kub9722@gmail.com>
2022-08-17 12:46:52 +0900

    feat: profile constants 추가

---
+++ src/constants/profile.ts
@@ -0,0 +1,72 @@
+export const profile = [
+  {
+    id: "1",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/20150525_31/klipk2_1432552616580KQKii_PNG/1.png?type=w2",
+  },
+  {
+    id: "2",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh8MHnD-7UsXCVw_QXxPC4Jm2q3p3MNzPt7cm2gAy9POfhQSX4ALuFUarQVvnLXMUyr9I&usqp=CAU",
+  },
+  {
+    id: "3",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODAxMDFfMTgy/MDAxNTE0ODEwMzA2NTU5.yLDFKLWN2AdSbQWMkOqsiTbKeJ4gFzBEy1ftsm0OLPsg.1dyRK6l3SJBowbbZeGIlC3WSsiU7l9VK6kxBoEore38g.JPEG.ssoreesia1102/downloadfile-27.jpg?type=w800",
+  },
+  {
+    id: "4",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODA4MjBfMjMw/MDAxNTM0NzMzMzY3Nzcw.YPA7nN0IyVwSFpNgMYPkqIKW7M8u0bkl3lp4sIYuYcwg.CE4wGyHleiby1vUHcIDKJvpciKYeza9iYSqATp-xOkEg.PNG.libridgeglobal/%EC%A0%9C%EC%9D%B4%ED%81%AC.png?type=w800",
+  },
+  {
+    id: "5",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/20150525_31/klipk2_1432552616580KQKii_PNG/1.png?type=w2",
+  },
+  {
+    id: "6",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODAxMDFfMTgy/MDAxNTE0ODEwMzA2NTU5.yLDFKLWN2AdSbQWMkOqsiTbKeJ4gFzBEy1ftsm0OLPsg.1dyRK6l3SJBowbbZeGIlC3WSsiU7l9VK6kxBoEore38g.JPEG.ssoreesia1102/downloadfile-27.jpg?type=w800",
+  },
+  {
+    id: "7",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODA4MjBfMjMw/MDAxNTM0NzMzMzY3Nzcw.YPA7nN0IyVwSFpNgMYPkqIKW7M8u0bkl3lp4sIYuYcwg.CE4wGyHleiby1vUHcIDKJvpciKYeza9iYSqATp-xOkEg.PNG.libridgeglobal/%EC%A0%9C%EC%9D%B4%ED%81%AC.png?type=w800",
+  },
+  {
+    id: "8",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/20150525_31/klipk2_1432552616580KQKii_PNG/1.png?type=w2",
+  },
+  {
+    id: "9",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODAxMDFfMTgy/MDAxNTE0ODEwMzA2NTU5.yLDFKLWN2AdSbQWMkOqsiTbKeJ4gFzBEy1ftsm0OLPsg.1dyRK6l3SJBowbbZeGIlC3WSsiU7l9VK6kxBoEore38g.JPEG.ssoreesia1102/downloadfile-27.jpg?type=w800",
+  },
+  {
+    id: "10",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODA4MjBfMjMw/MDAxNTM0NzMzMzY3Nzcw.YPA7nN0IyVwSFpNgMYPkqIKW7M8u0bkl3lp4sIYuYcwg.CE4wGyHleiby1vUHcIDKJvpciKYeza9iYSqATp-xOkEg.PNG.libridgeglobal/%EC%A0%9C%EC%9D%B4%ED%81%AC.png?type=w800",
+  },
+  {
+    id: "11",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/20150525_31/klipk2_1432552616580KQKii_PNG/1.png?type=w2",
+  },
+  {
+    id: "12",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODAxMDFfMTgy/MDAxNTE0ODEwMzA2NTU5.yLDFKLWN2AdSbQWMkOqsiTbKeJ4gFzBEy1ftsm0OLPsg.1dyRK6l3SJBowbbZeGIlC3WSsiU7l9VK6kxBoEore38g.JPEG.ssoreesia1102/downloadfile-27.jpg?type=w800",
+  },
+  {
+    id: "13",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxODA4MjBfMjMw/MDAxNTM0NzMzMzY3Nzcw.YPA7nN0IyVwSFpNgMYPkqIKW7M8u0bkl3lp4sIYuYcwg.CE4wGyHleiby1vUHcIDKJvpciKYeza9iYSqATp-xOkEg.PNG.libridgeglobal/%EC%A0%9C%EC%9D%B4%ED%81%AC.png?type=w800",
+  },
+  {
+    id: "14",
+    imgUrl:
+      "https://mblogthumb-phinf.pstatic.net/20150525_31/klipk2_1432552616580KQKii_PNG/1.png?type=w2",
+  },
+];
