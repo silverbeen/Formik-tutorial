8c74b30e64ed8793575bd1849827e58965c6db66
silverbeen <kub9722@gmail.com>
2022-08-19 14:25:54 +0900

    gitflow-feature-stash: test

---
+++ src/components/stream/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { StreamChat } from "stream-chat";
+
+const StreamChatting = () => {
+  const client = StreamChat.getInstance("m4jhq65dm6bn");
+              
+  useEffect(() => {
+    async () => {};
+  }, []);
+  return <div></div>;
+};
+
+export default StreamChatting;
